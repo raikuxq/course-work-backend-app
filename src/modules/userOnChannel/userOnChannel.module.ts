bb2421eb96fadd7f5a01f8c4a3570513d20fe995
Raiku <raikulmao.dev@gmail.com>
2023-04-13 07:01:18 +0300

    Fix userChannelsMemberOf

--- src/modules/userOnChannel/userOnChannel.module.ts
+++ src/modules/userOnChannel/userOnChannel.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserOnChannelService } from './userOnChannel.service';
+import { UserOnChannelResolver } from './userOnChannel.resolver';
 
 @Module({
-  providers: [UserOnChannelService],
-  exports: [UserOnChannelService],
+  providers: [UserOnChannelResolver, UserOnChannelService],
+  exports: [UserOnChannelResolver, UserOnChannelService],
 })
 export class UserOnChannelModule {}
